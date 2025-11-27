ae9779ad33c51b611e3500ed374aeda764713744
Salome.P <salome.poirson@telecomnancy.eu>
2025-11-27 17:23:36 +0100

    16 fini.

--- 2023/day16/solution1.py
+++ 2023/day16/solution1.py
@@ -5,46 +5,65 @@ with open("/home/etudiants/poirson48u/AOC-salome.poirson/2023/day16/input.txt", 
         line = line.rstrip("\n")
         tableau.append(list(line))
 
-print(tableau)
-print(len(tableau[0]))
-print(len(tableau))
+# print(tableau)
+# print(len(tableau[0]))
+# print(len(tableau))
 
-chemin = [[0 for i in range (110)] for i in range(110)]
-print(chemin)
-
-a_explorer=[(0,0,(0,1))]
-
-while a_explorer:
-    pass
-
-def next_direction(coord_x:int,coord_y:int,direction:tuple[int,int])->list[tuple[int,int]]:
+def next_direction(coord_y:int,coord_x:int,direction:tuple[int,int]):
     if tableau[coord_y][coord_x]==".":
-        return [direction]    
+        return [(coord_y+direction[0],coord_x+direction[1],direction)]    
     if tableau[coord_y][coord_x]=="-":
-        if direction[1]==0:
-            return [direction]
-        return[(coord_y,coord_x+1),(coord_y,coord_x-1)]
+        if abs(direction[1])==1:
+            return [(coord_y+direction[0],coord_x+direction[1],direction)]   
+        return[(coord_y,coord_x+1,(0,1)),(coord_y,coord_x-1,(0,-1))]
     if tableau[coord_y][coord_x]=="|":
-        if direction[0]==0:
-            return [direction]
-        return [(coord_y+1,coord_x),(coord_y-1,coord_x)]
+        if abs(direction[0])==1 :
+            return [(coord_y+direction[0],coord_x+direction[1],direction)]
+        return [(coord_y+1,coord_x,(1,0)),(coord_y-1,coord_x,(-1,0))]
     if tableau[coord_y][coord_x]=="/":
         if direction[0]==0:
             if direction[1]==1:
-                return [(-1,0)]
+                return [(coord_y-1,coord_x,(-1,0))]
             else:
-                return [(1,0)]
+                return [(coord_y+1,coord_x,(1,0))]
         elif direction[0]==1:
-            return [(0,-1)]
+            return [(coord_y,coord_x-1,(0,-1))]
         else:
-            return [(0,1)]
+            return [(coord_y,coord_x+1,(0,1))]
     if tableau[coord_y][coord_x]=="\\":
         if direction[0]==0:
             if direction[1]==1:
-                return [(1,0)]
+                return [(coord_y+1,coord_x,(1,0))]
             else:
-                return [(-1,0)]
+                return [(coord_y-1,coord_x,(-1,0))]
         elif direction[0]==1:
-            return [(0,1)]
+            return [(coord_y,coord_x+1,(0,1))]
         else:
-            return [(0,-1)]
+            return [(coord_y,coord_x-1,(0,-1))]
+    print("erreur")
+       
+def parcourir():
+    chemin = [[0 for i in range (110)] for i in range(110)] 
+    a_explorer=[(0,0,(0,1))]
+    deja_fait=[]
+
+    while a_explorer:
+        actuel = a_explorer[0]
+        if -1<=actuel[0]+actuel[2][0]<=110 and -1<=actuel[1]+actuel[2][1]<=110 and not (actuel in deja_fait):
+            a_explorer.extend(next_direction(actuel[0],actuel[1],actuel[2]))
+            chemin[actuel[0]][actuel[1]]=1
+            deja_fait.append(actuel)
+        a_explorer.remove(actuel)
+    return chemin
+
+resultat = parcourir()
+
+# for ligne in resultat:
+#     print("".join(map(str, ligne)))
+
+somme = 0
+for ligne in resultat:
+    for x in ligne:
+        somme += x
+
+print(somme)
